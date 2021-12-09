72ec1ad18d327059f81459e27fc66e52d755a3bb
Sa3edAhmed7 <95840200+Sa3edAhmed7@users.noreply.github.com>
2021-12-09 07:02:48 +0200

    Add files via upload

---
+++ Template Saeid/js/main.js
@@ -0,0 +1,29 @@
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false;
+
+window.onscroll = function () {
+
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+
+  if (window.scrollY >= statsSection.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
